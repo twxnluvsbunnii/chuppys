1a9fb423215c3a69ca7329ab09611624290bcc6a
twxnluvsbunnii <adrianna041205@gmail.com>
2026-09-17 06:57:08 -0400

    Update setup-metadata.js

--- api/setup-metadata.js
+++ api/setup-metadata.js
@@ -5,26 +5,7 @@ export default async function handler(req, res) {
     return res.status(401).send("Unauthorized");
   }
 
-  const response = await fetch(
-    `https://discord.com/api/v10/applications/${process.env.DISCORD_CLIENT_ID}/role-connections/metadata`,
-    {
-      method: "PUT",
-      headers: {
-        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify([
-        {
-          key: "verified",
-          name: "Verified",
-          description: "Has completed Chuppys verification",
-          type: 7
-        }
-      ])
-    }
-  );
-
-  const data = await response.text();
-
-  return res.status(response.status).send(data);
+  return res.status(200).json({
+    client_id: process.env.DISCORD_CLIENT_ID
+  });
 }
